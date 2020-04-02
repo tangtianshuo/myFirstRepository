a1943e1e0efe8193a6f78d645b073fe54ce5d5ba
tangtianshuo <59476439+tangtianshuo@users.noreply.github.com>
2020-04-02 16:49:05 +0800

    Revert "学习demo"
    This reverts commit fab543a011ce8207b646942fcf98c02d65b96671.

--- 02-app-b2c/src/main/java/com/codingstudy/b2c/dao/SysUserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.codingstudy.b2c.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.codingstudy.b2c.entity.SysUserEntity;
-
-public interface SysUserMapper extends BaseMapper<SysUserEntity> {
-
-}
